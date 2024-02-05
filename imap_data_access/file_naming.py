283f08c0adeb61f878d23f8a68661bd4f1c184e8
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-02-05 14:38:52 -0700

    Adding filename management
    Co-authored by: Laura Sandoval <laura.sandoval@lasp.colorado.edu>

---
+++ imap_data_access/file_naming.py
@@ -0,0 +1,33 @@
+"""Methods for managing and validating filenames."""
+import re
+
+def extract_filename_components(filename: str):
+    """
+    Extracts all components from filename.
+
+    Parameters
+    ----------
+    filename : str
+        Path of dependency data.
+
+    Returns
+    -------
+    components : dict
+        Dictionary containing components.
+
+    """
+    pattern = (
+        r"^imap_"
+        r"(?P<instrument>[^_]*)_"
+        r"(?P<datalevel>[^_]*)_"
+        r"(?P<descriptor>[^_]*)_"
+        r"(?P<startdate>\d{8})_"
+        r"(?P<enddate>\d{8})_"
+        r"(?P<version>v\d{2}-\d{2})"
+        r"\.(cdf|pkts)$"
+    )
+    match = re.match(pattern, filename)
+    if match is None:
+        return
+    components = match.groupdict()
+    return components
