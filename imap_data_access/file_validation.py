0109c1d70e5430194473ba3d5a22f2a16f9e0f17
Maxine Hartnett <maxine.hartnett@lasp.colorado.edu>
2024-02-06 11:05:54 -0700

    updates to file_validation

--- imap_data_access/file_validation.py
+++ imap_data_access/file_validation.py
@@ -1,13 +1,22 @@
 """Methods for managing and validating filenames."""
 import re
+import pathlib
 
-def extract_filename_components(filename: str):
+def extract_filename_components(filename: type[str | pathlib.Path]):
     """
     Extracts all components from filename.
 
+    Will return a dictionary with the following keys:
+    { instrument, datalevel, descriptor, startdate, enddate, version, extension }
+
+    Descriptor is an optional field. If it is not present in the filename, the dict
+    value for "descriptor" will be "".
+
+    If a match is not found, a ValueError will be raised.
+
     Parameters
     ----------
-    filename : str
+    filename : Pathlib.Path or str
         Path of dependency data.
 
     Returns
@@ -26,8 +35,12 @@ def extract_filename_components(filename: str):
         r"(?P<version>v\d{2}-\d{2})"
         r"\.(cdf|pkts)$"
     )
+    if isinstance(filename, pathlib.Path):
+        filename = filename.name
+
     match = re.match(pattern, filename)
     if match is None:
-        return
+        raise ValueError(f"Filename {filename} does not match expected pattern")
+
     components = match.groupdict()
     return components
