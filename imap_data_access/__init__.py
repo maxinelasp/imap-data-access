06c43000f9aed6dcc64ec91c1c7dc0b37e7b3bd9
Greg Lucas <greg.m.lucas@gmail.com>
2025-03-26 14:47:43 -0600

    REL: Updating version for v0.16 (#149)

--- imap_data_access/__init__.py
+++ imap_data_access/__init__.py
@@ -5,6 +5,7 @@ heliosphere. This package contains the data access tools for the IMAP mission. I
 provides a convenient way to query the IMAP data archive and download data files.
 """
 
+import importlib.metadata
 import os
 from pathlib import Path
 
@@ -30,8 +31,7 @@ __all__ = [
     "FILENAME_CONVENTION",
 ]
 
-__version__ = "0.15.1"
-
+__version__ = importlib.metadata.version("imap-data-access")
 
 config = {
     "DATA_ACCESS_URL": os.getenv("IMAP_DATA_ACCESS_URL")
